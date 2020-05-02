4dd77ba07017892a31de617631191cb253a85555
Talha <talhaanwar.anwar@gmail.com>
2020-05-02 16:37:04 +0530

    get userId from cookies leveraging chrome.cookies

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -11,25 +11,43 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 });
 
-console.log('chrome.storage', chrome.storage);
-console.log('session', document.cookie);
-
-var getCookie = function (cname) {
-  console.log('running');
-  console.log('document.cookie', document.cookie);
-  var name = cname + '=';
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return '';
+const getUserCookie = () => {
+  // for this to work
+  // permissions key in manifest should have
+  // cookies and url for url used (or url pattern)
+  return new Promise((resolve, reject) => {
+    const cookieDetails = {
+      url: 'https://angora.techpacker.io',
+      name: 'user',
+    };
+    chrome.cookies.get(cookieDetails, (cookie) => {
+      // chrome.cookies.get takes callback as 2nd parameter
+      // when callback returns we resolve
+      resolve(cookie);
+    });
+  });
 };
 
-console.log('cookie', getCookie('user'));
+const getUserId = async () => {
+  let userId = '';
+  // getUserCookie returns a Promise
+  // so using await to wait for it to return
+  const userCookie = await getUserCookie();
+  if (userCookie) {
+    // decode cookie value using decodeURIComponent
+    let userInfo = decodeURIComponent(userCookie.value);
+    // it is string so parse to object
+    userInfo = JSON.parse(userInfo);
+    userId = userInfo.id;
+  } else {
+    console.log(
+      'user cookie for angora.techpacker.io. Check if you are logged in'
+    );
+  }
+  return userId;
+};
+
+// get userId returns a Promise
+getUserId().then((userId) => {
+  console.log('userId:', userId);
+});
